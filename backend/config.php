f44f525d15ba1f4b0334720525d88aa05d253692
vao211 <vaolu211@gmail.com>
2025-03-27 03:37:38 +0700

    edit config.php for connect with username

--- backend/config.php
+++ backend/config.php
@@ -1,10 +1,24 @@
 <?php
-$conn = new mysqli("localhost:3306", "root", "", "jewelry_db");
+// $conn = new mysqli("localhost:3306", "root", "", "jewelry_db");
+// if ($conn->connect_error) {
+//     die("Kết nối thất bại: " . $conn->connect_error);
+// }
+
+// else{
+//     // echo"Connect Success";
+// }
+
+
+$servername = "Vubuntu.vinh.com";
+$username = "vao211"; 
+$password = "Vinh2112005";
+$dbname = "jewelry_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
-}
-
-else{
-    // echo"Connect Success";
+} else {
+    // echo "Kết nối thành công!";
 }
 ?>
