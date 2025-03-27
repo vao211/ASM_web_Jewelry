a3dc536f4ffc480ad9906c33ce0e013b54af056a
vao211 <vaolu211@gmail.com>
2025-03-27 17:11:57 +0700

    change txt to eng

--- frontend/user.php
+++ frontend/user.php
@@ -27,7 +27,7 @@ $user = $result->fetch_assoc();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trang Sức</title>
+    <title>Jewelry Store</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="/favicon.png" type="image/x-icon">
